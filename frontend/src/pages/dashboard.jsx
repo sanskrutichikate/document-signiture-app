419ad1031738e224caec63cb8276cf7042a8d15d
Sanskruti Chikate <sanskrutichikate6@gmail.com>
2026-06-11 22:40:54 +0530

    Day 7 complete

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 
 function Dashboard() {
     const [documents, setDocuments] = useState([]);
+    const [pdfFile, setPdfFile] = useState(null);
     const [selectedPdf, setSelectedPdf] = useState(null);
     const [signaturePos, setSignaturePos] = useState(null);
     const [selectedDocumentId, setSelectedDocumentId] = useState(null);
@@ -51,10 +52,53 @@ function Dashboard() {
             console.error(error);
         }
     };
+    const uploadPdf = async () => {
+        if (!pdfFile) {
+            alert("Please select a PDF");
+            return;
+        }
+
+        try {
+            const formData = new FormData();
+            formData.append("pdf", pdfFile);
+
+            const token = localStorage.getItem("token");
+
+            await axios.post(
+                "http://localhost:5000/api/documents/upload",
+                formData,
+                {
+                    headers: {
+                        Authorization: token,
+                        "Content-Type": "multipart/form-data",
+                    },
+                }
+            );
+
+            alert("PDF Uploaded Successfully");
+
+            fetchDocuments();
+
+        } catch (error) {
+            console.error(error);
+            alert("Upload Failed");
+        }
+    };
 
     return (
         <div>
             <h1>My Documents</h1>
+            <div>
+                <input
+                    type="file"
+                    accept=".pdf"
+                    onChange={(e) => setPdfFile(e.target.files[0])}
+                />
+
+                <button onClick={uploadPdf}>
+                    Upload PDF
+                </button>
+            </div>
 
             {/* DOCUMENT LIST */}
             {documents.map((doc) => (
